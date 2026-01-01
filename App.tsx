2f67cd52b769c758794151b969290674f07e0acc
axdamx <axdamx@gmail.com>
2026-01-01 14:05:26 +0800

    integrate app

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { StatusBar } from 'expo-status-bar';
-import RootNavigator from './src/navigation/RootNavigator';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { StatusBar } from "expo-status-bar";
+import RootNavigator from "./src/navigation/RootNavigator";
 
 // Create a query client instance
 const queryClient = new QueryClient({
